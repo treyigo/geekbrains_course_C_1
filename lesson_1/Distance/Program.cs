88e67b4ff6041b4dde4d518986aed6e7e582868a
tretyigo <igor.tret@gmail.com>
2020-05-02 22:45:28 +0300

    Первая итерация домашнего задания.

---
+++ lesson_1/Distance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Globalization;
+
+//Автор - студент Игорь Третьяков, задание "вычисление расстояния между двумя точками"
+class Program
+{
+    static void Main()
+    {
+        CultureInfo ci = new CultureInfo("ru");
+
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        Console.InputEncoding = Encoding.GetEncoding(1251);
+
+        Console.WriteLine("Введите координату Х первой точки");
+        double x1 = Convert.ToDouble(Console.ReadLine().Replace(".", ","));
+
+        Console.WriteLine("Введите координату Y первой точки");
+        double y1 = Convert.ToDouble(Console.ReadLine().Replace(".", ","));
+
+        Console.WriteLine("Введите координату X второй точки");
+        double x2 = Convert.ToDouble(Console.ReadLine().Replace(".", ","));
+
+        Console.WriteLine("Введите координату Y второй точки");
+        double y2 = Convert.ToDouble(Console.ReadLine().Replace(".", ","));
+
+        double r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        string formatR = r.ToString("f2", CultureInfo.InvariantCulture);
+        
+        Console.WriteLine($"Расстояние между точками составляет {formatR}");
+        Console.ReadKey();
+    }
+}
+
---
+++ lesson_1/form/Form.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+//Автор - студент Игорь Третьяков, задание "анкета"
+
+class Program
+{
+    static void Main()
+    {
+
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        Console.InputEncoding = Encoding.GetEncoding(1251);
+
+        Console.WriteLine("Введите ваше имя");
+        String name = Console.ReadLine();
+        Console.WriteLine(value:"Введите ваш возраст");
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Здравствуйте " + name + ", вам " + age + " лет.");
+        Console.WriteLine("Здравствуйте {0}, вам {1} лет.", name, age);
+        Console.WriteLine($"Здравствуйте {name}, вам {age} лет.");
+
+        Console.ReadKey();
+    }
+}
+
