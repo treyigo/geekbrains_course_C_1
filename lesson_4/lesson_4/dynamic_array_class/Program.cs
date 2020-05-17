739b16449f4e5348781b89338fecfcf41889794f
tretyigo <igor.tret@gmail.com>
2020-05-17 09:39:20 +0300

    Четвёртое домашнее задание

---
+++ lesson_4/lesson_4/dynamic_array_class/Program.cs
@@ -0,0 +1,109 @@
+﻿//студент Игорь Третьяков, класс для работы с массивом
+
+using System;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CoolArray
+{
+    class MyArray
+    {
+        private int[] a;
+        private int sum;
+        private int maxCount;
+        public MyArray(int size, int start, int step)
+        {
+            int value = start;
+            a = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                a[i] = value;
+                value += step;
+            }
+        }
+        public MyArray(int[] init)
+        {
+            int size = init.Length;
+            a = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                a[i] = init[i];
+            }
+
+        }
+        public int Max
+        {
+            get
+            {
+                return a.Max();
+            }
+        }
+        public int Sum
+        {
+            get
+            {
+                int sum = 0;
+                foreach (int el in a)
+                    sum += el;
+                return sum;
+            }
+        }
+        public int MaxCount
+        {
+            get
+            {
+                int maxCount = 0;
+                int maxValue = a.Max();
+                foreach (int el in a)
+                    if (el == maxValue)
+                    {
+                        maxCount++;
+                    }
+                return maxCount;
+            }
+        }
+
+        public void Multi(int k)
+        {
+            for (int i = 0; i < a.Length; i++)
+                a[i] *= k;
+        }
+
+        public MyArray Inverse()
+        {
+            MyArray copy = new MyArray(a);
+            for (int i = 0; i < a.Length; i++)
+                copy[i] *= -1;
+            return copy;
+        }
+
+        public int this[int i]
+        {
+            get { return a[i]; }
+            set { a[i] = value; }
+        }
+
+        public void Print()
+        {
+            foreach (int el in a)
+                Console.Write("{0,4}", el);
+            Console.WriteLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArray array1 = new MyArray(5, 0, 2);
+            array1.Print();
+            Console.WriteLine($"Сумма элементов:{array1.Sum}");
+            Console.WriteLine($"Количество максимальных элементов:{array1.MaxCount}");
+            array1.Multi(2);
+            array1.Print();
+            MyArray array2 = array1.Inverse();
+            array1.Print();
+            array2.Print();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lesson_4/lesson_4/static_array/Program.cs
@@ -0,0 +1,46 @@
+﻿//Студент Третьяков Игорь, статический класс для работы с массивом
+
+using System;
+using System.Data;
+
+static class StaticClass
+{
+    public static void FillRandomNumber(int[] a, int min, int max)
+    {
+        for (int i = 0; i<a.Length; i++)
+        {
+            Random rnd = new Random();
+            a[i] = rnd.Next(min, max);
+        }
+        //
+    }
+    public static int CountPairs(int[] a)
+    {
+        int count = 0;
+        for (int i = 0; i < a.Length-1; i++)
+        {
+            if ((a[i] % 3 == 0) ^ (a[i+1] % 3 == 0))   
+                count++;
+        }
+        return count;
+    }
+    public static void Print(int[] a)
+    {
+        for (int i = 0; i < a.Length; i++)
+            Console.Write("{0} ", a[i]);
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int[] testArray = new int[20];
+        StaticClass.FillRandomNumber(testArray, -10000, 10000);
+        StaticClass.Print(testArray);
+        int result = StaticClass.CountPairs(testArray);
+        Console.Write($"result:{result}");
+        Console.ReadKey();
+    }
+}
