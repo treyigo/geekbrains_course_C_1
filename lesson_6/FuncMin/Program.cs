7e630bec8bb4363fdc28ac17720ef8c7a65ccd1d
tretyigo <igor.tret@gmail.com>
2020-05-24 02:25:32 +0300

    Домашнее задание по 6 лекции

---
+++ lesson_6/FuncMin/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Globalization;
+
+namespace DoubleBinary
+{
+    public delegate double Fun(double x);
+
+    class Program
+    {
+
+        static double ReadNumber()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Console.InputEncoding = Encoding.GetEncoding(1251);
+            return Convert.ToDouble(Console.ReadLine().Replace(".", ","));
+        }
+
+        public static double F1(double x)
+        {
+            return x * x - 50 * x + 10;
+        }
+        public static void SaveFunc(string fileName, Fun F, double a, double b, double h)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            while (x <= b)
+            {
+                bw.Write(F(x));
+                x += h;// x=x+h;
+            }
+            bw.Close();
+            fs.Close();
+        }
+        public static double Load(string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            double min = double.MaxValue;
+            double d;
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                // Считываем значение и переходим к следующему
+                d = bw.ReadDouble();
+                if (d < min) min = d;
+            }
+            bw.Close();
+            fs.Close();
+            return min;
+        }
+        static void Main()
+        {
+            Fun[] functionsArray = {
+                delegate (double x) { return Math.Pow(x, 2); },
+                delegate (double x) { return Math.Sin(x); },
+                delegate (double x) { return Math.Exp(x); }
+            };
+            Console.WriteLine(@"Выберите функцию, введя eё номер:
+                1) х^2
+                2) sin(x)
+                3) exp(x)"
+);
+            int functionNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите стартовое значение анализируемого интревала:");
+            double start = ReadNumber();
+            Console.WriteLine("Введите конечное значение анализируемого интревала:");
+            double end   = ReadNumber();
+
+            SaveFunc("data.bin", functionsArray[functionNumber-1], start, end, 0.5);
+            Console.WriteLine($"Минимальное значение функции на интервале: {Load("data.bin")}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lesson_6/Table/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+// Описываем делегат. В делегате описывается сигнатура методов, на
+// которые он сможет ссылаться в дальнейшем (хранить в себе)
+public delegate double Fun(double x);
+public delegate double Fun2(double a, double x);
+
+class Program
+{
+    // Создаем метод, который принимает делегат
+    // На практике этот метод сможет принимать любой метод
+    // с такой же сигнатурой, как у делегата
+    public static void Table(Fun F, double x, double b)
+    {
+        Console.WriteLine("----- X ----- Y -----");
+        while (x <= b)
+        {
+            Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x, F(x));
+            x += 1;
+        }
+        Console.WriteLine("---------------------");
+    }
+    public static void Table2(Fun2 F2, double a, double x, double b)
+    {
+        Console.WriteLine("----- X ----- Y -----");
+        while (x <= b)
+        {
+            Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x, F2(a,x));
+            x += 1;
+        }
+        Console.WriteLine("---------------------");
+    }
+
+    // Создаем метод для передачи его в качестве параметра в Table
+    public static double MyFunc(double x)
+    {
+        return x * x * x;
+    }
+
+    // Создаем метод для передачи его в качестве параметра в Table
+    public static double MyFunc2(double a, double x)
+    {
+        return a * Math.Sin(x);
+    }
+    public static double MyFunc3(double a, double x)
+    {
+        return a * Math.Pow(x,2);
+    }
+
+    static void Main()
+    {
+        // Создаем новый делегат и передаем ссылку на него в метод Table
+        Console.WriteLine("Таблица функции 4*sin(x):");          
+        Table2(MyFunc2, 4, -2, 2);
+        Console.WriteLine("Таблица функции 3*x^2:");
+        // Упрощение(с C# 2.0). Использование анонимного метода
+        Table2(delegate (double a, double x) { return a *  Math.Pow(x, 2); }, 3, 0, 3);
+        Console.ReadKey();
+    }
+}
